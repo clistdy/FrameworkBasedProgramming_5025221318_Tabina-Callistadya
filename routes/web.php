cfc8ab592aaaec07669de16cf0700b4dfd37b243
Tabina Callistadya <115222811+clistdy@users.noreply.github.com>
2024-09-19 13:35:17 +0700

    Week-3

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,9 @@
 <?php
 
+use App\Models\Post;
+use Illuminate\Support\Arr;
 use Illuminate\Support\Facades\Route;
+
 
 Route::get('/', function () {
     return view('home', ['title' => 'HomePage']);
@@ -10,9 +13,17 @@ Route::get('/about', function () {
     return view('about', ['name' => 'Tabina Callistadya', 'title' => 'About']);
 });
 
-Route::get('/blog', function () {
-    return view('blog', ['title' => 'Blog']);
+Route::get('/posts', function () {
+    return view('posts', ['title' => 'Blog', 'posts'=> Post::all() ]);
 });
+
+Route::get('/posts/{slug}', function($slug){
+
+        $post = Post::find($slug);
+
+        return view('post', ['title' => 'Single Post', 'post' => $post]);
+});
+
 Route::get('/contact', function () {
     return view('contact', ['title' => 'Contact']);
 });

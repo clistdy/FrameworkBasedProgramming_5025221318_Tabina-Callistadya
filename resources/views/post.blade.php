05f22add0698a491242160b15caecdb442bfe97d
Tabina Callistadya <115222811+clistdy@users.noreply.github.com>
2024-10-02 13:52:24 +0700

    feat. added new model, factory, migration for post category

--- resources/views/post.blade.php
+++ resources/views/post.blade.php
@@ -3,9 +3,12 @@
         
     <article class="py-9 max-w-screen-md">
         <h2 class="mb-1 text-3xl tracking-tigh font-bold text-gray-900">{{ $post['title'] }}</h2>
-        <div class="text-base text-gray-500">
-            <a href="/authors/{{ $post->author->id }}">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans()}}
-        </div>
+        <div>
+            By
+            <a href="/authors/{{ $post->author->username}}"" class="text-base text-gray-500 hover:underline">{{ $post->author->name }}</a> | {{ $post->created_at->diffForHumans()}}
+            In
+            <a href="/categories/{{ $post->category->slug }}" class="text-base text-gray-500 hover:underline">{{ $post->category->name }}</a>
+       
         <p class="my-4 font-light">{{($post['body']) }}
         </p>
         <a href="/posts/" class="font-medium text-blue-500 hover:underline">&laquo; Back to posts</a>
